33333ba476cdf62a8d09334b419be40808ec4b7d
FlynnLee <593029372@qq.com>
2018-06-27 10:41:25 +0800

    add d.ts

---
+++ examples/index.js
@@ -0,0 +1,22 @@
+// @ts-check
+const { LightContainer } = require('light-container')
+
+class A {
+  constructor() {
+    this.a = 1
+  }
+}
+
+class B {
+  constructor() {
+    this.b = 2
+  }
+  /**
+   * @param {LightContainer} c
+   */
+  inject(c) {
+    this.a = c.get(A)
+  }
+}
+
+const c = new LightContainer().create([A, B])
---
+++ types/index.d.ts
@@ -0,0 +1,12 @@
+declare module 'light-container' {
+  interface Ctor_Static<T> {
+    new (...args): T
+  }
+  export class LightContainer {
+    new<T>(Ctor: Ctor_Static<T>, ...args): T
+    get<T>(Ctor: Ctor_Static<T>): T
+    create(providers: Array<{ new (...args) }>): this
+    bind<T>(Ctor: Ctor_Static<T>): this
+    has<T>(Ctor: Ctor_Static<T>): Boolean
+  }
+}
